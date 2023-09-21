2a9e05e9e5cb933fb25d2866ec71ac6e20d4e8f3
GoldOrange261 <63157008+GoldOrange261@users.noreply.github.com>
2023-09-22 06:55:04 +0800

    fix: a big blank between the title and the context

--- cogs/notification.py
+++ cogs/notification.py
@@ -70,10 +70,9 @@ class Notification(Cog_Extension):
             
 def gen_embed(tweet):
     author = tweet.author
-    embed=discord.Embed(title=f'{author.name} {get_action(tweet, disable_quoted=True)} {get_tweet_type(tweet)}', url=tweet.url, color=0x1da0f2, timestamp=tweet.created_on)
+    embed=discord.Embed(title=f'{author.name} {get_action(tweet, disable_quoted=True)} {get_tweet_type(tweet)}', description=tweet.text, url=tweet.url, color=0x1da0f2, timestamp=tweet.created_on)
     embed.set_author(name=f'{author.name} (@{author.username})', icon_url=author.profile_image_url_https, url=f'https://twitter.com/{author.username}')
     embed.set_thumbnail(url=author.profile_image_url_https[:-10]+'400x400.jpg')
-    embed.add_field(name='', value=tweet.text, inline=False)
     embed.set_footer(text='Twitter', icon_url='https://images-ext-2.discordapp.net/external/krcaH4psq2u8hROno0il7FE05UYL18EcpWwIekh0Vys/https/pingcord.xyz/assets/twitter-footer.png')
     if len(tweet.media) == 1:
         embed.set_image(url=tweet.media[0].media_url_https)
