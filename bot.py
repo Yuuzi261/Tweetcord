e8aed3089729aa6c325c1735a9732ad7c8734580
GoldOrange261 <63157008+GoldOrange261@users.noreply.github.com>
2023-09-23 00:15:54 +0800

    feat: use slash command to upload cookies and simplify the display of error handler

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from discord import app_commands
 from dotenv import load_dotenv
 import os
 import json
@@ -12,13 +13,19 @@ load_dotenv()
 
 bot = commands.Bot(command_prefix=os.getenv('PREFIX'), intents=discord.Intents.all())
 
+
 @bot.event
 async def on_ready():
     await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=os.getenv('ACTIVIY_NAME')))
+    await bot.tree.sync()
+    slash = await bot.tree.sync()
+    logger.info(f'loading {len(slash)} slash commands')
+    bot.tree.on_error = on_tree_error
     for filename in os.listdir('./cogs'):
             if filename.endswith('.py'):
                 await bot.load_extension(f'cogs.{filename[:-3]}')
     logger.info(f'{bot.user} is online')
+
 
 @bot.command()
 @commands.is_owner()
@@ -39,14 +46,15 @@ async def unload(ctx, extension):
 async def reload(ctx, extension):
     await bot.reload_extension(f'cogs.{extension}')
     await ctx.send(f'Re - Loaded {extension} done.')
-    
-    
-@bot.command()
+
+
 @commands.is_owner()
-@commands.dm_only()
-async def uploadCookies(ctx):
-    txtfile = await [attachment for attachment in ctx.message.attachments if attachment.filename[-4:] == '.txt'][0].read()
-    raw = json.loads(txtfile)
+@bot.tree.command(name = 'upload_cookies', description = 'upload necessary cookies')
+async def upload_cookies(itn : discord.Interaction, cookies : discord.Attachment):
+    info = await bot.application_info()
+    if itn.user != info.owner: raise commands.errors.NotOwner('only the owner can upload cookies')
+    cookies = await cookies.read()
+    raw = json.loads(cookies)
     needed_cookies = ['guest_id', 'guest_id_marketing', 'guest_id_ads', 'kdt', 'auth_token', 'ct0', 'twid', 'personalization_id']
     cookies = {}
     for cookie in raw:
@@ -54,26 +62,21 @@ async def uploadCookies(ctx):
         if name in needed_cookies: cookies[name] = cookie['value']
     with open('cookies.json', 'w') as f:
         json.dump(cookies, f)
+    await itn.response.send_message('successfully uploaded cookies', ephemeral = True)
     logger.info('successfully uploaded cookies')
         
 
 @bot.event
-async def on_command_error(ctx, error):
+async def on_tree_error(interaction : discord.Interaction, error : app_commands.AppCommandError):
+    await interaction.response.send_message(error.__context__, ephemeral = True)
+    logger.warning(f'an error occurred but was handled by the tree error handler, error message : {error}')
     
-    logger.warning(f'an error occurred but was handled by the error handler, error message : {error}')
     
-    embed=discord.Embed(title='ERROR', color=0xb7e0f3)
-    
-    if isinstance(error, commands.errors.CommandNotFound):
-        return
-    elif isinstance(error, commands.errors.PrivateMessageOnly):
-        embed.add_field(name='No Private Message', value='This command can only be used in private messages', inline=False)
-        embed.add_field(name='Error message', value=error, inline=False)
-    else:
-        embed.add_field(name='ERROR', value='An unexpected error occurred', inline=False)
-        embed.add_field(name='Error message', value=error, inline=False)
-
-    await ctx.send(embed=embed)
+@bot.event
+async def on_command_error(ctx : commands.context.Context, error : commands.errors.CommandError):
+    if isinstance(error, commands.errors.CommandNotFound): return
+    else: await ctx.send(error)
+    logger.warning(f'an error occurred but was handled by the command error handler, error message : {error}')
     
     
 if __name__ == '__main__':
