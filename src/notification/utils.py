09a09bc61af473231b33ad6fd67348a25e72623d
Yuuzi261 <lokasu0531@gmail.com>
2024-09-03 10:45:17 +0800

    refactor: small, unimportant changes

--- src/notification/utils.py
+++ src/notification/utils.py
@@ -1,5 +1,3 @@
-from src.utils import bool_to_str
-
 def is_match_type(tweet, enable_type: str):
     tweet_type = 0 if tweet.is_retweet else 1 if tweet.is_quoted else -1
     return tweet_type == -1 or enable_type[tweet_type] == '1'
